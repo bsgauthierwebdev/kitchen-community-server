38c3559cedeab26a2297304af87ac1f8706f91cc
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-18 19:59:13 -0500

    Updated seeds, config & server to attempt migrate

---
+++ seeds/seed.folders-and-recipes.sql
@@ -0,0 +1,12 @@
+TRUNCATE recipes, folders RESTART IDENTITY CASCADE;
+
+INSERT INTO folders (name)
+VALUES
+    ('Asian'),
+    ('BBQ'),
+    ('Comfort Food'),
+    ('Italian'),
+    ('Kitchen Hacks and Hints'),
+    ('Mediterranean'),
+    ('Seafood'),
+    ('Vegetarian / Vegan');
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: 'postgresql://dunder_mifflin@localhost/kitchen-community',
+    TEST_DATABASE_URL: 'postgresql://dunder_mifflin@localhost/kitchen-community-test'
 }
