6f5bc13b30f63289b990148534d7ad428f776681
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-21 13:15:58 -0500

    Edited server and postgrator-config

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const {PORT, DATABASE_URL, TEST_DATABASE_URL} = require('./config')
 
 const db = knex({
     client: 'pg',
-    connection: (process.env.NODE_ENV === 'production')
+    connection: (process.env.NODE_ENV === 'test')
         ? TEST_DATABASE_URL
         : DATABASE_URL,
 })
