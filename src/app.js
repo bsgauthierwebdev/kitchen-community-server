0668c2ce024163c845575de969b23693735bf4ee
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-23 18:31:21 -0500

    Changed cors setup

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
-const {CLIENT_ORIGIN} = require('./config')
+// const {CLIENT_ORIGIN} = require('./config')
 const folderRouter = require('./folders/folder-router')
 const recipeRouter = require('./recipes/recipe-router')
 
@@ -16,11 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
