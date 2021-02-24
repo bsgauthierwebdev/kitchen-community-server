730d3b7e58224aa062a2bd042f20e10473bfe287
Brent Gauthier <brentsgauthier@gmail.com>
2021-02-24 18:31:56 -0500

    Fixed null values in migrations

--- migrations/002.do.create_recipes.sql
+++ migrations/002.do.create_recipes.sql
@@ -3,12 +3,12 @@ CREATE TABLE recipes (
     name TEXT NOT NULL,
     modified TIMESTAMPTZ DEFAULT now() NOT NULL,
     description TEXT NOT NULL,
-    prepTime TEXT,
-    cookTime TEXT,
-    servings INTEGER,
-    ingredients TEXT,
+    "prepTime" TEXT NOT NULL,
+    "cookTime" TEXT NOT NULL,
+    servings INTEGER NOT NULL,
+    ingredients TEXT NOT NULL,
     directions TEXT NOT NULL,
-    folderId INTEGER
+    "folderId" INTEGER
         REFERENCES folders(id) ON DELETE CASCADE NOT NULL
 );
 
@@ -25,7 +25,7 @@ VALUES
     ('Seafood'),
     ('Vegetarian / Vegan');
 
-INSERT INTO recipes (name, description, prepTime, cookTime, servings, ingredients, directions, folderId)
+INSERT INTO recipes (name, description, "prepTime", "cookTime", servings, ingredients, directions, "folderId")
 VALUES
     ('Sweet Potato Power Bowl', 'Vegan meets Southwest', '10 minutes', '30 minutes', 4, 'Cubed sweet potato, olive oil, canned black beans, chopped onion, bell pepper, garlic, chili powder, adobo powder, oregano, hominy, vegetable stock', 'Heat oil and saute pepper, onion and garlic until soft. Add seasonings and allow to toast in oil. Add sweet potato and stock, cover and steam 10 minutes. Add black beans and heat through. Taste for seasoning. Add hominy and bring to temp.', 8),
     ('Asian Test Seed', 'Testing Seeds', '10 seconds', '10 seconds', 1, 'Words', 'Put text and integers in the proper place', 1);
